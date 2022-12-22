d210408d4b82948c23406de76e54a116db48467e
Rahul Solanki <rahul.solanki.12032001@gmail.com>
2022-12-22 11:37:06 +0530

    Add resume name

--- scripts/script1.js
+++ scripts/script1.js
@@ -119,7 +119,7 @@ function DownloadFile(fileName) {
                     var url = window.URL || window.webkitURL;
                     link = url.createObjectURL(blob);
                     var a = document.createElement("a");
-                    a.setAttribute("download", fileName);
+                    a.setAttribute("download", "Rahulj9aResume.pdf");
                     a.setAttribute("href", link);
                     document.body.appendChild(a);
                     a.click();
